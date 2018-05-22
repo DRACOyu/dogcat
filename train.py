ef2a87b4965bf5a386d302cbd8e01da01889cd6f
DRACOyu <34015789+DRACOyu@users.noreply.github.com>
2018-05-22 18:18:11 +0800

    data
    data

---
+++ train.py
@@ -0,0 +1,170 @@
+# %%
+
+import os
+import numpy as np
+import tensorflow as tf
+import input_data
+import model
+
+# %%
+
+N_CLASSES = 2
+IMG_W = 180  # resize the image, if the input image is too large, training will be very slow.
+IMG_H = 180
+BATCH_SIZE = 8
+CAPACITY = 1000
+MAX_STEP = 30000  # with current parameters, it is suggested to use MAX_STEP>10k
+learning_rate = 0.0001  # with current parameters, it is suggested to use learning rate<0.0001
+
+
+# %%
+def run_training():
+    # you need to change the directories to yours.
+    train_dir = '/home/draco/桌面/untitled/data/train/'
+    logs_train_dir = '/home/draco/桌面/untitled/data/train/'
+
+    train, train_label = input_data.get_files(train_dir)
+    train_batch, train_label_batch = input_data.get_batch(train,
+                                                          train_label,
+                                                          IMG_W,
+                                                          IMG_H,
+                                                          BATCH_SIZE,
+                                                          CAPACITY)
+    train_logits = model.inference(train_batch, BATCH_SIZE, N_CLASSES)
+    train_loss = model.losses(train_logits, train_label_batch)
+    train_op = model.trainning(train_loss, learning_rate)
+    train_acc = model.evaluation(train_logits, train_label_batch)
+
+    # train_batch, train_label_batch = input_data.get_batch(train,
+    #                                                       train_label,
+    #                                                       IMG_W,
+    #                                                       IMG_H,
+    #                                                       BATCH_SIZE,
+    #                                                       CAPACITY)
+    # train_logits = model.inference(train_batch, BATCH_SIZE, N_CLASSES)
+    # train_loss = model.losses(train_logits, train_label_batch)
+    # train_op = model.trainning(train_loss, learning_rate)
+    # train_acc = model.evaluation(train_logits, train_label_batch)
+
+    summary_op = tf.summary.merge_all()
+    sess = tf.Session()
+    train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+    saver = tf.train.Saver()
+
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+
+    try:
+        for step in np.arange(MAX_STEP):
+            if coord.should_stop():
+                break
+            _, tra_loss, tra_acc = sess.run([train_op, train_loss, train_acc])
+
+            if step % 100 == 0:
+                print("Step %d, train loss = %.2f, train accuracy = %.2f%%" % (step, tra_loss, tra_acc))
+                summary_str = sess.run(summary_op)
+                train_writer.add_summary(summary_str, step)
+            if step % 2000 == 0 or (step + 1) == MAX_STEP:
+                checkpoint_path = os.path.join(logs_train_dir, "model.ckpt")
+                saver.save(sess, checkpoint_path, global_step=step)
+    except tf.errors.OutOfRangeError:
+        print("Done training -- epoch limit reached.")
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
+    sess.close()
+
+
+
+# train
+# run_training()
+
+# %% Evaluate one image
+# when training, comment the following codes.
+
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def get_one_image(train):
+   '''Randomly pick one image from training data
+   Return: ndarray
+   '''
+   n = len(train)
+   ind = np.random.randint(0, n)
+   img_dir = train[ind]
+
+   image = Image.open(img_dir)
+   plt.imshow(image)
+   image = image.resize([180, 180])
+   image = np.array(image)
+   return image
+
+
+
+def evaluate_one_image(image_array):
+   '''Test one image against the saved models and parameters
+   '''
+
+   # you need to change the directories to yours.
+
+   with tf.Graph().as_default():
+       BATCH_SIZE = 1
+       N_CLASSES = 2
+
+       image = tf.cast(image_array, tf.float32)
+       image = tf.image.per_image_standardization(image)
+       image = tf.reshape(image, [1, 180, 180, 3])
+       logit = model.inference(image, BATCH_SIZE, N_CLASSES)
+
+       logit = tf.nn.softmax(logit)
+
+       x = tf.placeholder(tf.float32, shape=[180, 180, 3])
+       print(1)
+       # you need to change the directories to yours.
+       logs_train_dir = '/home/draco/桌面/untitled/data/train/'
+
+       saver = tf.train.Saver()
+
+       with tf.Session() as sess:
+
+           print("Reading checkpoints...")
+           ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+           if ckpt and ckpt.model_checkpoint_path:
+               global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+               saver.restore(sess, ckpt.model_checkpoint_path)
+               print('Loading success, global_step is %s' % global_step)
+           else:
+               print('No checkpoint file found')
+
+           prediction = sess.run(logit, feed_dict={x: image_array})
+           max_index = np.argmax(prediction)
+           if max_index==0:
+               print('This is a cat with possibility %.6f' %prediction[:, 0])
+           else:
+               print('This is a dog with possibility %.6f' %prediction[:, 1])
+train_dir = '/home/draco/桌面/untitled/data/train/'
+train, train_label = input_data.get_files(train_dir)
+image_array1 = get_one_image(train)
+image_array2 = get_one_image(train)
+evaluate_one_image(image_array1)
+evaluate_one_image(image_array2)
+
+
+plt.imshow(image_array1)
+plt.show()
+
+plt.imshow(image_array2)
+plt.show()
+
+
+
+# %%
+#Expected image (JPEG, PNG, or GIF), got unknown format starting with '\n\213>\022\350=\n:\n\003Add\022\006\n'
+	# [[Node: DecodeJpeg = DecodeJpeg[acceptable_fraction=1, channels=3, dct_method="",
+    # fancy_upscaling=true, ratio=1, try_recover_truncated=false, _device="/job:localhost/replica:0/task:0/device:CPU:0"](ReadFile)]]
+
+
